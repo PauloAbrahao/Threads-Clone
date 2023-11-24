f512e9e4a0737c78b732ed3b5f745572134daca9
PauloAbrahao <ab.cod@outlook.com>
2023-11-24 17:42:07 -0300

    feat: implement profile page

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -5,12 +5,11 @@ import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
-
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
-
-  if (!userInfo.onboarded) redirect("/onboarding");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
+import Thread from "../models/thread.models";
 import { connectToDB } from "../mongoose";
 
 interface updateUserProps {
@@ -55,5 +56,32 @@ export async function fetchUser(userId: string) {
     // });
   } catch (err: any) {
     throw new Error(`Failed to fetch user: ${err.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
   }
 }
