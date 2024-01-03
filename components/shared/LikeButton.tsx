da3622bd33587d3313a848ce50972b38fa5c5117
PauloAbrahao <ab.cod@outlook.com>
2024-01-03 19:53:15 -0300

    feat: working on like thread

--- components/shared/LikeButton.tsx
+++ components/shared/LikeButton.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { dislikeThread, likeThread } from "@/lib/actions/thread.actions";
+import {
+  dislikeThread,
+  isUserLiked,
+  likeThread,
+} from "@/lib/actions/thread.actions";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const LikeButton = ({
   threadId,
@@ -15,16 +19,29 @@ const LikeButton = ({
   currentUserId: string;
   userLiked: string;
 }) => {
-  // State to track the liked status
-
   const [count, setCount] = useState<number>(likeCount || 0);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Atualiza o estado com o número mais recente de likes do banco de dados
+    setCount(likeCount || 0);
+
+    // Verifica se o usuário atual curtiu o post
+    isUserLiked(threadId, currentUserId)
+      .then((result) => setIsLiked(result))
+      .catch((error) =>
+        console.error("Erro ao verificar like:", error.message)
+      );
+  }, [likeCount, userLiked, currentUserId, threadId]);
 
   // Handler function for liking a thread
   const handleLikeButton = async (threadId: string, currentUserId: string) => {
     try {
       await likeThread(threadId, currentUserId);
 
-      setCount(count);
+      // Atualiza o estado com o novo valor de likeCount e marca como curtido
+      setCount((prevCount) => prevCount + 1);
+      setIsLiked(true);
     } catch (error: any) {
       console.error("Erro ao atualizar like:", error.message);
     }
@@ -38,7 +55,9 @@ const LikeButton = ({
     try {
       await dislikeThread(threadId, currentUserId);
 
-      setCount(count);
+      // Atualiza o estado com o novo valor de likeCount e marca como não curtido
+      setCount((prevCount) => Math.max(prevCount - 1, 0));
+      setIsLiked(false);
     } catch (error: any) {
       console.error("Erro ao atualizar deslike:", error.message);
     }
@@ -47,19 +66,15 @@ const LikeButton = ({
   return (
     <button
       onClick={
-        userLiked.toString() === currentUserId.toString()
+        isLiked
           ? () => handleDislikeButton(threadId, currentUserId)
           : () => handleLikeButton(threadId, currentUserId)
       }
     >
       <div className="flex flex-row justify-between gap-1 items-center">
         <Image
-          src={
-            userLiked === currentUserId
-              ? "/assets/heart-filled.svg"
-              : "/assets/heart-gray.svg"
-          }
-          alt={userLiked === currentUserId ? "heart-filled" : "heart-gray"}
+          src={isLiked ? "/assets/heart-filled.svg" : "/assets/heart-gray.svg"}
+          alt={isLiked ? "heart-filled" : "heart-gray"}
           width={24}
           height={24}
           className="cursor-pointer object-contain"
